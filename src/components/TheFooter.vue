39972cbfc85ec8665acccd93e88003a4c8582ec2
David Jesus <davidsilvajesus@hotmail.com>
2023-06-26 09:15:52 -0300

    Criação do componente Footer da aplicação.

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-footer app class="bg-blue-darken-3">
+        <v-row>
+          <v-col class="d-flex justify-center">
+            <span >Copyright 2022 - <strong>Top Way</strong></span>
+          </v-col>
+        </v-row>
+  </v-footer>
+</template>
