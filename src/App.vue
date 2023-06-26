971c814049fa523463a2f36787f047757f9b207c
David Jesus <davidsilvajesus@hotmail.com>
2023-06-26 09:05:40 -0300

    Estrutura o App.vue.
    - Nesse arquivo foi feito a estrutura da SPA para utilizar o vue
    router para redirecionamento de pagina e gerenciamento de estado
    com o pinia para exibição da snackbar globalmente sem a necessidade
    de importar o componente em todas as telas.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
-<script setup lang="ts"></script>
 
 <template>
   <v-app>
     <v-main>
-      <v-container class="fill-height">
+      <v-container fluid class="fill-height">
         <router-view></router-view>
       </v-container>
     </v-main>
+    <snackbar/>
   </v-app>
 </template>
 
+<script setup lang="ts">
+import snackbar from "@/components/SnackBar.vue"
+</script>
 <style scoped></style>
