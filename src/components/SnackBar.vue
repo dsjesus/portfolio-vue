3829d8d294f86360fa7d6034cc4f4156d6282847
David Jesus <davidsilvajesus@hotmail.com>
2023-06-26 09:10:40 -0300

    Cria o componente snackBar.
    - Nesse componente foi feita uma snackBar aonde ela recebe por
    props o texto que sera exibido, o tipo status para mudar a cor
    do componente, mas quem esta gerenciando esse componente é o
    pinia que altera o estado e atualiza o componente.

---
+++ src/components/SnackBar.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <v-snackbar v-model="alert.display" :color="alert.type" :timeout="timeout" absolute location="bottom right">
+    <div>
+      <v-icon class="mr-1" icon="mdi-lock" :color="alert.type"></v-icon>
+      {{ alert.text }}
+    </div>
+  </v-snackbar>
+</template>
+<script setup lang="ts">
+import { useAlertsStore } from "@/stores"
+import { storeToRefs } from 'pinia'; 
+
+const store = useAlertsStore()
+const {alert} = storeToRefs(store)
+
+defineProps({
+  display: {
+    type: Boolean,
+    require: Boolean,
+    default: false
+  },
+  text: {
+    type: String,
+    default: ''
+  },
+  type: {
+    type: String,
+    default: 'error'
+  },
+  timeout: {
+    type: [Number, String],
+    default: 5000
+  }
+})
+
+</script>
