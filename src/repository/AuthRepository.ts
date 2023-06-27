abcb8ec704f2ae017f44990351e33ecaa9314892
David Jesus <davidsilvajesus@hotmail.com>
2023-06-27 14:42:08 -0300

    Repository de teste

--- src/repository/AuthRepository.ts
+++ src/repository/AuthRepository.ts
@@ -1,11 +1,11 @@
 export const authUser = (username, password: any) => {
   return new Promise((res, rej) => {
     setTimeout(() => {
-      if (username == 'david' && password == '12345') {
+      if (username == 'teste' && password == 'teste') {
         return res({
           data: {
-            name: 'david silva de jesus',
-            username: 'david jesus',
+            name: 'Teste',
+            username: 'Teste',
             token: 'a545as-564asd564-a4s5d487',
             message: 'Login efetuado com sucesso.',
           }
